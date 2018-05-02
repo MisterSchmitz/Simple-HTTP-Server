d653f371b2753bceeeb74dfd850edc292591098b
Mike <misterschmitz1@gmail.com>
2018-05-02 10:40:41 -0700

    threads

--- src/httpd.cpp
+++ src/httpd.cpp
@@ -4,6 +4,7 @@
 #include <stdlib.h>     /* for atoi() and exit() */
 #include <string.h>     /* for memset() */
 #include <unistd.h>     /* for close() */
+#include <pthread.h>
 #include <iostream>
 #include "httpd.hpp"
 
@@ -12,21 +13,26 @@
 
 using namespace std;
 
-void start_httpd(unsigned short port, string doc_root)
+string doc_root;
+
+struct ThreadArgs {
+    int clntSock;
+};
+
+void start_httpd(unsigned short port, string root_arg)
 {
 	cerr << "Starting server (port: " << port <<
-		", doc_root: " << doc_root << ")" << endl;	
+		", doc_root: " << root_arg << ")" << endl;
+
+	doc_root = root_arg;
 
     int servSock;                    /* Socket descriptor for server */
-    int clntSock;                    /* Socket descriptor for client */
-    struct sockaddr_in echoServAddr; /* Local address */
-    struct sockaddr_in echoClntAddr; /* Client address */
-    unsigned int clntLen;            /* Length of client address data structure */
-
     /* Create socket for incoming connections */
     if ((servSock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0)
         DieWithError("socket() failed");
-      
+
+    struct sockaddr_in echoServAddr; /* Local address */
+
     /* Construct local address structure */
     memset(&echoServAddr, 0, sizeof(echoServAddr));   /* Zero out structure */
     echoServAddr.sin_family = AF_INET;                /* Internet address family */
@@ -43,21 +49,50 @@ void start_httpd(unsigned short port, string doc_root)
         DieWithError("listen() failed");
 	printf("Server listening on port %i\n", port);
 
+    int clntSock;  /* Socket descriptor for client */
+    struct sockaddr_in echoClntAddr; /* Client address */
+    unsigned int clntLen;            /* Length of client address data structure */
+    /* Set the size of the in-out parameter */
+    clntLen = sizeof(echoClntAddr);
     for (;;) /* Run forever */
     {
-        /* Set the size of the in-out parameter */
-        clntLen = sizeof(echoClntAddr);
-
         /* Wait for a client to connect */
-        if ((clntSock = accept(servSock, (struct sockaddr *) &echoClntAddr, 
-                               &clntLen)) < 0)
+        if ((clntSock = accept(servSock, (struct sockaddr *) &echoClntAddr,
+                               &clntLen)) < 0) {
             DieWithError("accept() failed");
+        }
 
         /* clntSock is connected to a client! */
-        // printf("Handling client %s\n", inet_ntoa(echoClntAddr.sin_addr));
+        printf("Handling client %s\n", inet_ntoa(echoClntAddr.sin_addr));
 
-        HandleTCPClient(clntSock, doc_root);
+        /* Create separate memory for client argument */
+        struct ThreadArgs *threadArgs = (struct ThreadArgs *) malloc(
+                sizeof(struct ThreadArgs));
+        if (threadArgs == NULL) {
+            DieWithError("malloc() failed");
+        }
+        threadArgs->clntSock = clntSock;
+
+        /* Create client thread */
+        pthread_t threadId;
+        int returnValue = pthread_create(&threadId, NULL, ThreadMain, threadArgs);
+        if (returnValue != 0) {
+            DieWithError("pthread_create() failed");
+        }
+        printf("with thread %lu\n", (unsigned long int) threadId);
+
+//        HandleTCPClient(clntSock, doc_root);
     }
     /* NOT REACHED */
 }
 
+void *ThreadMain(void *threadArgs) {
+    pthread_detach(pthread_self());
+
+    int clntSock = ((struct ThreadArgs *) threadArgs)->clntSock;
+    free(threadArgs);
+
+    HandleTCPClient(clntSock, doc_root);
+
+    return (NULL);
+}
--- src/httpd.hpp
+++ src/httpd.hpp
@@ -7,13 +7,15 @@
 #include <stdlib.h>     /* for atoi() and exit() */
 #include <string.h>     /* for memset() */
 #include <unistd.h>     /* for close() */
+#include <pthread.h>
 
 using namespace std;
 
-void start_httpd(unsigned short port, string doc_root);
+void start_httpd(unsigned short port, string root_arg);
 void DieWithError(const char *errorMessage);  /* Error handling function */
 void HandleTCPClient(int clntSocket, string doc_root);   /* TCP client handling function */
 int CreateTCPServerSocket(unsigned short port); /* Create TCP server socket */
 int AcceptTCPConnection(int servSock);  /* Accept TCP connection request */
+void *ThreadMain(void *arg);
 
 #endif // HTTPD_H
